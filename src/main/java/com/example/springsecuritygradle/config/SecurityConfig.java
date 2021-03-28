73c2b6d8749b92eb0562ee343065a7f6cf9faa7d
Simon <wonjin@gmail.com>
2021-03-28 16:44:10 +0900

    Customize configuration

--- src/main/java/com/example/springsecuritygradle/config/SecurityConfig.java
+++ src/main/java/com/example/springsecuritygradle/config/SecurityConfig.java
@@ -1,2 +1,33 @@
-package com.example.springsecuritygradle.config;public class SecurityConfig {
+package com.example.springsecuritygradle.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .mvcMatchers("/", "/info").permitAll()
+                .mvcMatchers("/admin").hasRole("ADMIN")
+                .anyRequest().authenticated();
+
+        http.formLogin()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("{noop}1q2w3e4r").roles("ADMIN")
+                .and()
+                .withUser("simon").password("{noop}wonjinism").roles("USER");
+    }
 }
--- src/main/java/com/example/springsecuritygradle/form/SampleController.java
+++ src/main/java/com/example/springsecuritygradle/form/SampleController.java
@@ -1,2 +1,39 @@
-package com.example.springsecuritygradle.form;public class SampleController {
+package com.example.springsecuritygradle.form;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+
+@Controller
+public class SampleController {
+
+    @GetMapping("/")
+    public String index(Model model, Principal principal) {
+        if (principal == null) {
+            model.addAttribute("message", "Hello, Simon?");
+        } else {
+            model.addAttribute("message", "Hello, " + principal.getName());
+        }
+        return "index";
+    }
+
+    @GetMapping("/info")
+    public String info(Model model) {
+        model.addAttribute("message", "Hello, Simon?");
+        return "info";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard(Model model, Principal principal) {
+        model.addAttribute("message", "Hello, " + principal.getName());
+        return "dashboard";
+    }
+
+    @GetMapping("/admin")
+    public String admin(Model model, Principal principal) {
+        model.addAttribute("message", "Hello, Admin" + principal.getName());
+        return "admin";
+    }
 }
