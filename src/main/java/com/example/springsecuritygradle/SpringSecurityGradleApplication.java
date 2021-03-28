890295baeb0b3e49fe318f918326e8d35cfaf8e5
Simon <wonjin@gmail.com>
2021-03-28 15:43:40 +0900

    init project

---
+++ src/main/java/com/example/springsecuritygradle/SpringSecurityGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritygradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityGradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityGradleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springsecuritygradle/SpringSecurityGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritygradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityGradleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
